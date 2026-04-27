8a1c79574ae60f6772b140a7b57970a12d61e57b
francismoreto <morets132@gmail.com>
2026-04-27 09:28:07 +0800

    04.27.26 Redirection and updated worker

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/', permanent=False)),
     path("admin/", admin.site.urls),
     path('', include('app.urls')),
 
